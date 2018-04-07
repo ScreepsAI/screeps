94251c7f3d45d0eec3333a33879ec07f9f1950ca
jjj201200 <jjj201200@gmail.com>
2018-04-07 15:00:47 +0800

    fuck ts 11

--- src/init.ts
+++
@@ -1,7 +0,0 @@
-import { InitManager } from './managers';
-export const init = () => {
-	if (!Memory.Config || !Memory.Config.init) {
-		// 初始化manager
-		InitManager();
-	}
-};
--- src/loop.ts
+++ src/loop.ts
@@ -1,41 +1,5 @@
-import _ from 'lodash';
-import { init } from './init';
 import { ErrorMapper } from './utils/ErrorMapper';
-import { getUsername } from './utils';
-// 注入 prototypes 并注册新的 global 项目，使用 isRoot 进行检测是否需要重新注入
-// ==========================================================================
-const Root = (): void => {
-	if (_.isUndefined(global.isRoot) || _.isUndefined(Memory.config)) {
-		console.log(String.fromCodePoint(0x1f503), 'Code Reloading ...');
-
-		// Assign config
-		global._ME = getUsername();
-		if (_.isUndefined(Memory.config)) Memory.config = {};
-		// 不要改 config 引用路径（打包时候不会引入）
-		_.assign(Memory.config, require('config'));
-		_.assign(global, Memory.config);
-
-		// Extend game prototypes
-		require('./prototypes');
-
-		// Extend functions
-		require('./global');
-
-		init();
-
-		// Checkpoint
-		global.isRoot = true;
-		Log.success('Root Done');
-		// Memory.Clocks = {};
-		// const func = function() {
-		// 	Log.info(this.params.counter++);
-		// 	if (this.params.counter > 10) {
-		// 		this.restart();
-		// 	}
-		// };
-		// new Clock('test clock', { counter: 0 }, func, 1, true);
-	}
-};
+import { reboot, needReboot } from './reboot';
 
 // Main Loop
 // ==========================================================================
@@ -46,6 +10,6 @@ const Loop = (): void => {
 // 解析 SourceMap , 统一错误处理
 // ==========================================================================
 export default ErrorMapper.wrapLoop((): void => {
-	Root();
+	if (needReboot) reboot();
 	Loop();
 });
